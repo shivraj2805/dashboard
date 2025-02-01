bb4e75f87bc8ac92d17fd683d917e858cc2fa90f
shivraj2805 <darekar1005@gmail.com>
2025-02-02 00:25:03 +0530

    Change link

--- src/components/BuyActionWindow.jsx
+++ src/components/BuyActionWindow.jsx
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:8080/newOrder", {
+    axios.post("https://full-stack-trading-platform-backend.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
